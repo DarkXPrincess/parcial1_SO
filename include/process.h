e57074ff93c9cc56143d584accf094c629a477ac
veronica granados <veronica.granados@correounivalle.edu.co>
2025-10-18 22:51:46 -0500

    version 1.0

---
+++ include/process.h
@@ -0,0 +1,14 @@
+#ifndef PROCESS_H
+#define PROCRESS_H
+
+#include <string>
+using namespace std;
+
+struct process{
+    string id;
+    int bt, at, q ,pr;
+    int remaining;
+    int wt=0, ct=0, rt=-1, tat=0;
+};
+
+#endif
