5609df0a19e1ba7e0895191dcc88e20fb01b83da
veronica granados <veronica.granados@correounivalle.edu.co>
2025-10-19 22:27:32 -0500

    ultima version

--- include/process.h
+++ include/process.h
@@ -1,15 +1,10 @@
 #ifndef PROCESS_H
-#define PROCRESS_H
-
+#define PROCESS_H
 #include <string>
-using namespace std;
-
-struct Process{
-    string id;
-    int bt, at, q ,pr;
+struct Process {
+    std::string id;
+    int bt, at, q, pr;
     int remaining;
-    int wt=0, ct=0, rt=-1, tat=0;
+    int wt = 0, ct = 0, rt = -1, tat = 0;
 };
-
 #endif
-
